15ee8966ed9b36f3f0068b28b41555e1021016e1
DKatsarou <92544325+DKatsarou@users.noreply.github.com>
2021-12-08 21:52:06 +0200

    Update Entity.java

--- mazegame/src/main/java/gr/aueb/dmst/mazegame/Entity.java
+++ mazegame/src/main/java/gr/aueb/dmst/mazegame/Entity.java
@@ -1,10 +1,10 @@
 package gr.aueb.dmst.mazegame;
 import java.awt.image.*;
 public class Entity {
-    
-    public int x, y;
-    public int speed;
-
-    public BufferedImage up1;
-    public String direction;
+    public int lx, ly;
+	public int speed;
+	public BufferedImage up1;
+	public String direction;
+	public Rectangle solidArea; 
+	public boolean collisionOn = false;
 }
