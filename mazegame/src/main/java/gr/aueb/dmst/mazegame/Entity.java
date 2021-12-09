26a4ed1f1005aa4b78e23b6f4af47efc6a031c4b
DKatsarou <92544325+DKatsarou@users.noreply.github.com>
2021-12-09 21:49:20 +0200

    Update Entity.java

--- mazegame/src/main/java/gr/aueb/dmst/mazegame/Entity.java
+++ mazegame/src/main/java/gr/aueb/dmst/mazegame/Entity.java
@@ -2,7 +2,7 @@ package gr.aueb.dmst.mazegame;
 import java.awt.image.*;
 import java.awt.Rectangle;
 public class Entity {
-    public int lx, ly;
+    public int lx, ly;//labyrinth x and y
 	public int speed;
 	public BufferedImage up1;
 	public String direction;
