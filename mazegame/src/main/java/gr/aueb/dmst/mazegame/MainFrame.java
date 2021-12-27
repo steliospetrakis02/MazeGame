b63eac7465ba9269c3083c189cbd79ee2e5fe9b2
Stelios Petrakis <92467926+SteliosPetrakis02@users.noreply.github.com>
2021-12-27 20:43:45 +0200

    Update MainFrame.java
    first unit test

--- mazegame/src/main/java/gr/aueb/dmst/mazegame/MainFrame.java
+++ mazegame/src/main/java/gr/aueb/dmst/mazegame/MainFrame.java
@@ -21,7 +21,11 @@ public class MainFrame extends javax.swing.JFrame {
     public MainFrame() {
         initComponents();
     }
-
+    //first test
+    public static String test() {
+        return "test";
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
