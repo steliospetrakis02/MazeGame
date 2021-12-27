8b73f906ddd79a7db045348736293e569dc3aecd
Stelios Petrakis <92467926+SteliosPetrakis02@users.noreply.github.com>
2021-12-27 20:31:55 +0200

    Create test23.java

---
+++ mazegame/src/test/java/gr/aueb/dmst/mazegame/test23.java
@@ -0,0 +1,13 @@
+
+package gr.aueb.dmst.mazegame;
+import gr.aueb.dmst.mazegame.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class test23 {
+
+    @Test
+    public void testtt() {
+        assertEquals("test", MainFrame.test());
+        assertEquals("tes3123t", MainFrame.test());
+    }
+}
