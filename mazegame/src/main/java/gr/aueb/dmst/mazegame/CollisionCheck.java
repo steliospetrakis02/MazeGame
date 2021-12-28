1922ad62f1010debf8b329aac6aeb6b0cc53a872
DKatsarou <92544325+DKatsarou@users.noreply.github.com>
2021-12-28 23:18:27 +0200

    Update CollisionCheck.java

--- mazegame/src/main/java/gr/aueb/dmst/mazegame/CollisionCheck.java
+++ mazegame/src/main/java/gr/aueb/dmst/mazegame/CollisionCheck.java
@@ -61,4 +61,73 @@ public class CollisionCheck {
 			break;
 		}
 	}
+	//CHECKING IF THE PLAYER IS HITTING AN OBJ
+	public int checkObj(Entity entity,boolean player) {
+		int index = 999;
+		
+		for(int i = 0; i < gp.obj.length; i++) {
+			if(gp.obj[i] != null) {
+				//entity's solid area position
+				entity.solidArea.x = entity.lx + entity.solidArea.x;
+				entity.solidArea.y = entity.ly + entity.solidArea.y;
+				//object's solid area position
+				gp.obj[i].solidArea.x = gp.obj[i].worldX + gp.obj[i].solidArea.x;
+				gp.obj[i].solidArea.y = gp.obj[i].worldY + gp.obj[i].solidArea.y;
+			
+				switch(entity.direction) {
+				case "up":
+					entity.solidArea.y -= entity.speed;
+					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						if(gp.obj[i].collision == true) {
+							entity.collisionOn = true;
+						}
+						if(player == true) {
+							index = i;
+						}
+					}
+					break;
+				case "down":
+					entity.solidArea.y += entity.speed;
+					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						if(gp.obj[i].collision == true) {
+							entity.collisionOn = true;
+						}
+						if(player == true) {
+							index = i;
+						}
+					}
+					break;
+				case "left":
+					entity.solidArea.x -= entity.speed;
+					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						if(gp.obj[i].collision == true) {
+							entity.collisionOn = true;
+						}
+						if(player == true) {
+							index = i;
+						}
+					}
+					break;
+				case "right":
+					entity.solidArea.x += entity.speed;
+					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						if(gp.obj[i].collision == true) {
+							entity.collisionOn = true;
+						}
+						if(player == true) {
+							index = i;
+						}
+					}
+					break;
+				}
+				entity.solidArea.x = entity.solidAreaDefaultX;
+				entity.solidArea.y = entity.solidAreaDefaultY;
+			    gp.obj[i].solidArea.x = gp.obj[i].solidAreaDefaultX;
+			    gp.obj[i].solidArea.y = gp.obj[i].solidAreaDefaultY;
+			}
+		}
+		
+		
+		return index;
+	}
 }
