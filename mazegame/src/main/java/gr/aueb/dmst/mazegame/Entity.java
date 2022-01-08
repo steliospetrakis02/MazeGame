c22908c8a673297869532cd86131c305a10b3175
DKatsarou <92544325+DKatsarou@users.noreply.github.com>
2022-01-08 11:03:55 +0200

    Update Entity.java

--- mazegame/src/main/java/gr/aueb/dmst/mazegame/Entity.java
+++ mazegame/src/main/java/gr/aueb/dmst/mazegame/Entity.java
@@ -4,8 +4,10 @@ import java.awt.Rectangle;
 public class Entity {
     public int lx, ly;//labyrinth x and y
 	public int speed;
-	public BufferedImage up1;
+	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
 	public String direction;
+	public int spriteCounter = 0;
+	public int spriteNum = 1;
 	public Rectangle solidArea; 
 	public int solidAreaDefaultX, solidAreaDefaultY;
 	public boolean collisionOn = false;
